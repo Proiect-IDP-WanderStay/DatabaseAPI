c500ed824c40d1f2b004cebab07950bf08cd6b3a
geografie <georgianaortila13@gmail.com>
2024-05-12 13:14:52 +0300

    Added order function

--- project/helpers/oder.py
+++ project/helpers/oder.py
@@ -1,3 +1,6 @@
+from typing import List
+from ..models import Room, Hotel, Contact
+
 class Order:
     def __init__(self) -> None:
         self.alphabetic = 1
@@ -11,3 +14,21 @@ class Order:
             self.alphabetic = json['alphabetic']
         if ('price' in json):
             self.price = json['price']
+
+    def helper_order(self, room):
+        hotel = Hotel.query.filter_by(id=room.hotel_id).first()
+        return hotel.name
+
+    def order_rooms(self, rooms: List[Room]) -> List[Room]:
+        if self.alphabetic:
+            rooms.sort(key=lambda room: self.helper_order(room))
+        else:
+            rooms.sort(key=lambda room: self.helper_order(room), reverse=True)
+
+        if self.price:
+            rooms.sort(key=lambda room: room.price)
+        else:
+            rooms.sort(key=lambda room: room.price, reverse=True)
+        return rooms
+
+
